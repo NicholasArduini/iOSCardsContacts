830bb17b4f93cd8af30e187b45dffe8879c559ed
Nicholas Arduini <arduininicholas@gmail.com>
2019-12-31 09:52:32 -0500

    Add favourite button to card detail

--- Cards Contacts/Controllers/CardDetailParentViewController.swift
+++ Cards Contacts/Controllers/CardDetailParentViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class CardDetailParentViewController: UIViewController {
     
+    @IBOutlet weak var favouriteButton: UIBarButtonItem!
+    
     var uid : String?
+    var isFavourite = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.makeNavTransparent()
+    }
+    
+    @IBAction func favouriteButton(_ sender: Any) {
+        self.isFavourite = !self.isFavourite
+        if isFavourite {
+            self.favouriteButton.image = UIImage(named: Constants.STAR_FILLED_IMAGE)
+        } else {
+            self.favouriteButton.image = UIImage(named: Constants.STAR_OPENED_IMAGE)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
