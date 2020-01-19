a1d3befc1f0fd7feb2e3785eed5bcdc7bf48db6e
Nicholas Arduini <arduininicholas@gmail.com>
2020-01-18 22:04:57 -0500

    Animate login fields up when keyboard is brought in view

--- Cards Contacts/Controllers/New Group/LoginViewController.swift
+++ Cards Contacts/Controllers/New Group/LoginViewController.swift
@@ -13,9 +13,18 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
-        
-    override func viewDidLayoutSubviews() {
+    
+    @IBOutlet weak var logoImageTopContraint: NSLayoutConstraint!
+    @IBOutlet weak var emailFieldToLogoImageTopContraint: NSLayoutConstraint!
+    var defaultLogoContraint: CGFloat = 0
+    
+    override func viewDidLoad() {
         self.setupView()
+        self.registerForKeyboardNotifications()
+    }
+    
+    deinit {
+        self.deregisterFromKeyboardNotifications()
     }
     
     func setupView() {
@@ -25,6 +34,7 @@ class LoginViewController: UIViewController {
         
         loginButton.makeRounded()
         self.hideKeyboardWhenTappedAround()
+        self.defaultLogoContraint = CGFloat(self.logoImageTopContraint.constant)
     }
     
     @IBAction func loginButton(_ sender: Any) {
@@ -36,6 +46,32 @@ class LoginViewController: UIViewController {
             }, onFailure: {
                 self.loginButton.loadingIndicator(false)
             })
+        }
+    }
+    
+    func registerForKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func deregisterFromKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWasShown(notification: NSNotification){
+        UIView.animate(withDuration: 0.4) {
+            self.logoImageTopContraint.constant = 16
+            self.emailFieldToLogoImageTopContraint.constant = 16
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func keyboardWillBeHidden(notification: NSNotification){
+        UIView.animate(withDuration: 0.4) {
+            self.logoImageTopContraint.constant = self.defaultLogoContraint
+            self.emailFieldToLogoImageTopContraint.constant = self.defaultLogoContraint
+            self.view.layoutIfNeeded()
         }
     }
 }
